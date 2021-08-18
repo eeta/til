f26fc2c176df3eff4cef96e1ab9ba93e0dc4cb43
eeta <hwlim.me@gmail.com>
2021-08-18 14:26:29 +0900

    Use auto configuration

--- src/main/java/me/eeta/springboot/Application.java
+++ src/main/java/me/eeta/springboot/Application.java
@@ -1,13 +1,27 @@
 package me.eeta.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+
+
+/*
+@Configuration
+@Component
+@EnableAutoConfiguration
+*/
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication application = new SpringApplication(Application.class);
+		// application.setWebApplicationType(WebApplicationType.NONE);
+		application.run(args);
 	}
 
 }
